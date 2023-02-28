329161cc581b596ce65594440b5816432454cff1
Ouranoboros <pierre.moutiers@gmail.com>
2023-02-28 13:24:15 +0100

    Fin de cours Mardi

--- Travail/convert.c
+++ Travail/convert.c
@@ -1,26 +1,36 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int gd_atoi(char *str)
 {
-    int i, neg, ret;
+    int i, neg, result;
     i = 0;
+    neg = 1;
+    result = 0;
 
     while (str[i])
     {
-        if ((str[i] >= 43 && str[i] <= 59) || (str[i] == 43 || str[i] == 45))
+        if ((str[i] >= 43 && str[i] <= 57) || (str[i] == 43 || str[i] == 45))
             break;
+        i++;
     }
     if (str[i] == 43 || str[i] == 45)
     {
         if (str[i] == 45)
             neg = -1;
+        i++;
     }
-    i++;
-    int value = str[i] - 48;
-    return (ret * neg);
+    while (str[i])
+    {
+        int value = str[i] - 48;
+        result = result * 10 + value;
+        i++;
+    }
+    return (result * neg);
 }
 
 int main(void)
 {
-    gd_atoi("-5000");
+    int nb_put = gd_atoi("nhhnrth-512154");
+    printf("%i\n", nb_put);
 }
