183212aa5a19d450b476f87b20903a27f5433f44
Ouranoboros <pierre.moutiers@gmail.com>
2023-03-01 18:06:21 +0100

    includes.h correction

--- Fonction_en_C/includes.h
+++ Fonction_en_C/includes.h
@@ -11,18 +11,18 @@
 // Nom de mes fonctions
 
 // Conversion array to int
-int gd_atoi(char *str)
-    // Fusionne deux strings
-    char *gd_join(char const *s1, char const *s2)
-    // Ecrit un caractère
-    void gd_putchar(char c)
-    // Convertit des int en string de façon récursive
-    void gd_putnbr_rec(int nb_put)
-    // Convertit des int en string avec de la mémoire
+int gd_atoi(char *str);
+// Fusionne deux strings
+char *gd_join(char const *s1, char const *s2);
+// Ecrit un caractère
+void gd_putchar(char c);
+// Convertit des int en string de façon récursive
+void gd_putnbr_rec(int nb_put);
+// Convertit des int en string avec de la mémoire
 
-    // Copie une string dans une variable avec de la mémoire
-    char *gd_strdup(char const *src)
-    // Longueur d'une string
-    int gd_strlen(char const *str)
+// Copie une string dans une variable avec de la mémoire
+char *gd_strdup(char const *src);
+// Longueur d'une string
+int gd_strlen(char const *str);
 
 #endif
